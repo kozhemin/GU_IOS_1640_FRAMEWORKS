0dd9a842b90ee2a35a46a4437430d0187e891077
Egor.Kozhemin <egor.kozhemin@gmail.com>
2022-05-15 00:38:52 +0300

    git init

---
+++ GBFrameworks/PrivateServiceKey.swift
@@ -0,0 +1,10 @@
+//
+//  PrivateServiceKey.swift
+//  GBFrameworks
+//
+//  Created by Егор Кожемин on 14.05.2022.
+//
+
+import Foundation
+
+let GMS_API_KEY = "AIzaSyB7TIrwlUBq-v6O3UkXs08xsi2V5HTlL0A"
